b6bbb98d8ddfa6f5d3f260939490f0ffb235c369
Praveen <ppk763957@gmail.com>
2026-01-20 21:57:17 +0530

    Resources added

--- Day 1/Oddeven.java
+++ Day 1/Oddeven.java
@@ -1,13 +1,12 @@
 
 public class Oddeven {
     public static void main(String[] args) {
-        int a=4;
-        if(a%2==0){
-            System.out.println("a is even number");    
-        }
-        else{
+        int a = 4;
+        if (a % 2 == 0) {
+            System.out.println("a is even number");
+        } else {
             System.out.println("a is odd number");
         }
-        
+
     }
 }
